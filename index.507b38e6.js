226a9d6fc1d500b24a3b48342c18661fde5e5c37
lublubuterbrodi <lublubuterbrodi@gmail.com>
2024-10-07 14:26:59 +0300

    make build

---
+++ index.507b38e6.js
@@ -0,0 +1,2 @@
+var t=document.querySelector(".spider");window.onload=function(){t.style.left="50%",t.style.top="50%",t.style.position="absolute",t.style.transform="translate(-50%, -50%)"};
+//# sourceMappingURL=index.507b38e6.js.map
